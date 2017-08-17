94fea0e79ddd53e7b99f79bec1ed26076e267a46
Fernando Pereira Lisboa <fernandopereiralisboa@gmail.com>
2017-08-17 17:37:30 -0300

    Created count function

--- backend/src/api/message/messageService.js
+++ backend/src/api/message/messageService.js
@@ -2,4 +2,14 @@ const Message = require('./message')
 
 Message.methods(['get', 'post', 'delete'])
 
+Message.route('count', (req, res, next) => {
+    Message.count((error, value) => {
+        if(error) {
+            res.status(500).json({errors: [error]})
+        } else {
+            res.json({value})
+        }
+    })
+})
+
 module.exports = Message
