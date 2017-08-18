0593cda86dc3a4962e01567c8aeccab045fee7ee
Fernando Pereira Lisboa <fernandopereiralisboa@gmail.com>
2017-08-18 12:16:31 -0300

    Fixed webpack version

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,11 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+
+import React from 'react'
+import Sms from '../sms/sms'
+
+export default props => (
+    <div className='container'>
+        <Sms />
+    </div>
+)
---
+++ frontend/src/sms/sms.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Sms extends Component {
+    render () {
+        return (
+            <div>
+                <h1>SMS</h1>
+            </div>
+        )
+    }
+}
